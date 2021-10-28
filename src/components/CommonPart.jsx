6e84916009651a299598bd5b66ecc7c98a11fadf
Samrat Biswas <samratb601@gmail.com>
2021-10-28 23:20:12 +0530

    first react web

--- src/components/CommonPart.jsx
+++ src/components/CommonPart.jsx
@@ -9,7 +9,7 @@ return(
     <div className="container-fluid pt-4 "><div className="row"><div className="col col-10 mx-auto my-4">
         <div className="row ">
 
-        <div className="d-flex flex-column justify-content-center col-md-6 col-sm-12 pt-4 pt-lg-0 mx-sm-auto order-lg-0 order-sm-1 ">
+        <div className="d-flex flex-column justify-content-center col-md-6 col-sm-12 pt-4 pt-lg-0 mx-sm-auto order-lg-0 order-md-0 order-1 ">
             <h1 className="ms-0">Welcome to <span className="fw-bold text-success"> {props.page} </span> Page</h1>
             <h2 className="h6 my-3">{props.pageInfo}</h2>
             <div className="my-3">
@@ -17,7 +17,7 @@ return(
             </div>
         </div>
 
-        <div className=" col-md-6 col-sm-12 pt-4 my-sm-5 order-lg-1 order-sm-0 " id="home_img_div">
+        <div className=" col-md-6 col-sm-12 pt-4 my-sm-5 order-lg-1 order-0 " id="home_img_div">
             <img className="img-fluid" id="home_img" src={ props.imgSrc } alt="Home img"/>
         </div>
             
