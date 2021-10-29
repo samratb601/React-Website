ae6d678c8d7529ea0b1d5b82e6e1144e8a356152
Samrat Biswas <samratb601@gmail.com>
2021-10-29 13:00:16 +0530

    Updates

--- src/ErrorPage.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Error = () => {
-    return(<h2>This is Error Page</h2>)
-}
-
-export default Error;
--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-const Footer = () => {
-    let currentYear = new Date().getFullYear();
-    return (<footer className="footer text-center  py-2" id="footer">Copyright @ { currentYear }</footer>)
-}
-
-export default Footer;
