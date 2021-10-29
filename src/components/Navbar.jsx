c16be0dd70d1a44fd2827ca6824e317b34f7903e
Samrat Biswas <samratb601@gmail.com>
2021-10-29 13:08:28 +0530

    navbar title change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                     <div className="col-10 mx-auto">
                     <nav className="navbar navbar-expand-lg navbar-light ">
                     <div className="container-fluid">
-                    <NavLink className="navbar-brand fw-bold " to="/">Navbar</NavLink>
+                    <NavLink className="navbar-brand fw-bold " to="/">SamB</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
