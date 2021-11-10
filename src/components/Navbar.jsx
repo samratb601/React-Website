fceb50ff9254de1ccee4bba780a90e932c24476e
Samrat Biswas <samratb601@gmail.com>
2021-11-10 14:50:29 +0530

    navbar changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import MenuOpenOutlinedIcon from '@material-ui/icons/MenuOpenOutlined';
 const Navbar = () => {
     return (<div className="container-fluid nav_bg " id="sticky_nav">
                 <div className="row">
-                    <div className="col-11  mx-auto">
+                    <div className="col-12 col-md-11  mx-auto">
                     <nav className="navbar navbar-expand-lg navbar-light shadow-sm ">
                     <div className="container-fluid">
                     <NavLink className="navbar-brand fw-bold " id="navbar-brand" to="/">XYZ</NavLink>
@@ -16,16 +16,16 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <NavLink exact activeClassName="active_link"  className="nav-link d-md-inline-block "  to="/">Home</NavLink>
+                            <NavLink exact activeClassName="active_link"  className="nav-link d-inline-block  "  to="/">Home</NavLink>
                                 </li>
                                 <li className="nav-item">
-                            <NavLink  activeClassName="active_link" className="nav-link d-md-inline-block "  to="/about">About</NavLink>
+                            <NavLink  activeClassName="active_link" className="nav-link d-inline-block  "  to="/about">About</NavLink>
                                 </li>
                                 <li className="nav-item">
-                            <NavLink  activeClassName="active_link" className="nav-link d-md-inline-block "  to="/service">Serivce</NavLink>
+                            <NavLink  activeClassName="active_link" className="nav-link d-inline-block  "  to="/service">Serivce</NavLink>
                                 </li>
                                 <li className="nav-item">
-                            <NavLink  activeClassName="active_link" className="nav-link d-md-inline-block "  to="/contact">Contact</NavLink>
+                            <NavLink  activeClassName="active_link" className="nav-link d-inline-block  "  to="/contact">Contact</NavLink>
                                 </li>        
                         </ul>
                     </div>
